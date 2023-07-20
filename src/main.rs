80dc3797a437f660e872020a547900d1b489f339
AlexanderMaxRanabel <93342430+AlexanderMaxRanabel@users.noreply.github.com>
2023-07-20 21:57:57 +0300

    Fixed filterer

--- src/main.rs
+++ src/main.rs
@@ -32,7 +32,7 @@ async fn discover(url: String, wordlist: String) -> Result<(), Box<dyn std::erro
     Ok(())
 }
 
-async fn discover_extract(url: String, wordlist: String) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+async fn discover_filter(url: String, wordlist: String) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let file = File::open(wordlist).expect("Failed to open the file.");
 
     // Create a BufReader to read the file efficiently
@@ -43,19 +43,10 @@ async fn discover_extract(url: String, wordlist: String) -> Result<(), Box<dyn s
         if let Ok(word) = line {
             let target = url.clone().to_string() + &*word.to_string();
             let response = reqwest::get(target.clone()).await?;
-            let status = response.status().to_string();
             //let resp = response.json::<HashMap<String, String>>().await?;
-
-            let code:Option<&str> = status.split_whitespace().nth(0);
-            let result = match code {
-                Some(code) => code.to_string(),
-                None => String::from("Unknown"),
-            };
-
-            if result == "200" {
-                println!("{} {} {}", result.green(), word, target.clone().magenta());
-            } else {
-                println!("Why");
+            if response.status().is_success() {
+                // Only print URLs with successful responses (status code 200)
+                println!("{} {} {}", "200".green(), word, target.clone().magenta());
             }
         }
     }
@@ -66,22 +57,22 @@ async fn discover_extract(url: String, wordlist: String) -> Result<(), Box<dyn s
 async fn main() {
     let args: Vec<String> = env::args().collect();
     //monologue0 --url1 https://discord.com/2 --wordlist3 test.txt4  --extract5
-    if args.len() > 4 {
+    if args.len() > 5 {
         let argument = &args[1].to_string();
         match argument.as_str() {
             "--url" => {
                 let url = &args[2].to_string();
                 let wordlist = &args[4].to_string();
-                if args.len() > 6 {
-                    let extractor = &args[5].to_string();
-                    if extractor == "--extract" {
-                        let _ = tokio::task::spawn(discover_extract(url.clone(),wordlist.clone())).await;
-                    } else {
-                        println!("Unknown")
-                    }
+                let mode = &args[5].to_string();
+                match mode.as_str() {
+                    "--normal" => {
+                        let _ = tokio::task::spawn(discover(url.clone(), wordlist.clone())).await;
+                    },
+                    "--filter" => {
+                        let _ = tokio::task::spawn(discover_filter(url.clone(),wordlist.clone())).await;
+                    },
+                    _ => println!("Unknown mode")
                 }
-
-                let _ = tokio::task::spawn(discover(url.clone(), wordlist.clone())).await;
             },
 
             "help" => {

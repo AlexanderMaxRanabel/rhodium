38b86d7456d62ecca259110cd77420cd11d1b0b6
AlexanderMaxRanabel <93342430+AlexanderMaxRanabel@users.noreply.github.com>
2023-07-21 11:45:38 +0300

    Improved Filtiratio

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # monologue
-A Content Discovery Tool insipired from Feroxbuster. Work In Progress
+A Content Discovery Tool written in Rust, insipired from Feroxbuster.
+
+## Installation
+### Dependencies
+- OpenSSL (If You are on linux).
+- Rust programming language and its toolkit.
+- Git
+
+### Building
+1. Clone the git repository using ```git clone https://github.com/AlexanderMaxRanabel/monologue.git```
+2. cd into the directory.
+3. run ```cargo build --release```
+4. Executable should be at target/release. Install a wordlist and start using Monologue.
+
+## Licensing
+Monologue is licensed under GNU Affero General Public License 3.0. See LICENSE for more information.
--- src/main.rs
+++ src/main.rs
@@ -32,7 +32,7 @@ async fn discover(url: String, wordlist: String) -> Result<(), Box<dyn std::erro
     Ok(())
 }
 
-async fn discover_filter(url: String, wordlist: String, filteree: String) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+async fn discover_filter(url: String, wordlist: String, filteree: String, type_fiter: String) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let file = File::open(wordlist).expect("Failed to open the file.");
 
     // Create a BufReader to read the file efficiently
@@ -41,45 +41,85 @@ async fn discover_filter(url: String, wordlist: String, filteree: String) -> Res
     // Iterate over each line in the wordlist
     for line in reader.lines() {
         if let Ok(word) = line {
-            let target = url.clone().to_string() + &*word.to_string();
-            let response = reqwest::get(target.clone()).await?;
-            let status = response.status().to_string();
-            //let resp = response.json::<HashMap<String, String>>().await?;
+            match type_fiter.as_str() {
+                "--bycode" => {
+                    let target = url.clone().to_string() + &*word.to_string();
+                    let response = reqwest::get(target.clone()).await?;
+                    let status = response.status().to_string();
+                    //let resp = response.json::<HashMap<String, String>>().await?;
 
-            let code:Option<&str> = status.split_whitespace().nth(0);
-            let result = match code {
-                Some(code) => code.to_string(),
-                None => String::from("Unknown"),
-            };
+                    let code:Option<&str> = status.split_whitespace().nth(0);
+                    let result = match code {
+                        Some(code) => code.to_string(),
+                        None => String::from("Unknown"),
+                    };
+                    match filteree.as_str() {
+                        "200" => {
+                            if response.status().is_success() {
+                                println!("{} {} {}", result.green(), word, target.clone().magenta());
+                            }
+                        },
+                        "404" | "401" | "402" | "403" => {
+                            if response.status().is_client_error() {
+                                println!("{} {} {}", result.green(), word, target.clone().magenta());
+                            }
+                        },
 
-            match filteree.as_str() {
-                "200" => {
-                    if response.status().is_success() {
-                        // Only print URLs with successful responses (status code 200)
-                        println!("{} {} {}", result.green(), word, target.clone().magenta());
+                        "301" | "307" | "302" => {
+                            if response.status().is_redirection() {
+                                println!("{} {} {}", result.green(), word, target.clone().magenta());
+                            }
+                        },
+
+                        "500" | "502" => {
+                            if response.status().is_server_error() {
+                                println!("{} {} {}", result.green(), word, target.clone().magenta());
+                            }
+                        },
+                        _ => println!("Unknown Status code")
                     }
                 },
 
-                "404" | "400" | "401" | "402" | "403" => {
-                    if response.status().is_client_error() {
-                        println!("{} {} {}", result.green(), word, target.clone().magenta());
-                    }
-                },
+                "--bytype" => {
+                    let target = url.clone().to_string() + &*word.to_string();
+                    let response = reqwest::get(target.clone()).await?;
+                    let status = response.status().to_string();
+                    //let resp = response.json::<HashMap<String, String>>().await?;
 
-                "301" | "307" | "302" => {
-                    if response.status().is_redirection() {
-                        println!("{} {} {}", result.green(), word, target.clone().magenta());
-                    }
-                },
+                    let code:Option<&str> = status.split_whitespace().nth(0);
+                    let result = match code {
+                        Some(code) => code.to_string(),
+                        None => String::from("Unknown"),
+                    };
+                    match filteree.as_str() {
+                        "success" => {
+                            if response.status().is_success() {
+                                println!("{} {} {}", result.green(), word, target.clone().magenta());
+                            }
+                        },
 
-                "500" | "502" => {
-                    if response.status().is_redirection() {
-                        println!("{} {} {}", result.green(), word, target.clone().magenta());
+                        "client_error" => {
+                            if response.status().is_client_error() {
+                                println!("{} {} {}", result.green(), word, target.clone().magenta());
+                            }
+                        },
+
+                        "redirection" => {
+                            if response.status().is_redirection() {
+                                println!("{} {} {}", result.green(), word, target.clone().magenta());
+                            }
+                        },
+
+                        "server_error" => {
+                            if response.status().is_server_error() {
+                                println!("{} {} {}", result.green(), word, target.clone().magenta());
+                            }
+                        },
+                        _ => println!("Unknown Type")
                     }
                 },
-                _ => println!("Unknown Status code")
+                _ => println!("Unknown Type")
             }
-
         }
     }
     Ok(())
@@ -88,7 +128,7 @@ async fn discover_filter(url: String, wordlist: String, filteree: String) -> Res
 #[tokio::main]
 async fn main() {
     let args: Vec<String> = env::args().collect();
-    //monologue0 --url1 https://discord.com/2 --wordlist3 test.txt4  --extract5
+    //monologue0 --url1 https://discord.com/2 --wordlist3 test.txt4  --filter5 --bycode6 4047
     if args.len() > 5 {
         let argument = &args[1].to_string();
         match argument.as_str() {
@@ -101,8 +141,9 @@ async fn main() {
                         let _ = tokio::task::spawn(discover(url.clone(), wordlist.clone())).await;
                     },
                     "--filter" => {
-                        let filteree = &args[6].to_string();
-                        let _ = tokio::task::spawn(discover_filter(url.clone(),wordlist.clone(), filteree.clone())).await;
+                        let filteree = &args[7].to_string();
+                        let type_filter = &args[6].to_string();
+                        let _ = tokio::task::spawn(discover_filter(url.clone(),wordlist.clone(), filteree.clone(), type_filter.clone())).await;
                     },
                     _ => println!("Unknown mode")
                 }
@@ -115,6 +156,6 @@ async fn main() {
             _  => println!("Unknown Argument. Please --help"),
         }
     } else {
-        println!("No argument provided");
+        println!("Insufficient Arguments");
     }
 }
